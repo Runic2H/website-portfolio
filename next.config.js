c686473d786aca7c18748620295ebf478811b415
Elton Teo <eltonteo99@gmail.com>
2025-02-02 01:31:02 +0800

    revert: edit next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['your-image-domains'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
 }
 
 module.exports = nextConfig
