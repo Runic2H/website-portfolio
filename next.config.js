3c5c583acd73b2f9e40daab4c9586b4bf1076923
Elton Teo <eltonteo99@gmail.com>
2025-02-01 16:03:23 +0800

    test: fix for vercel hosting

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['runic2h-website.vercel.app/'],
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    domains: ['media.githubusercontent.com/media/Runic2H/website-portfolio/refs/heads/main/public/'],
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
 }
 
 module.exports = nextConfig
