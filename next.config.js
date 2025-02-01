0628c194ca728d26716bd95697eb9a8ed38956e6
Elton Teo <eltonteo99@gmail.com>
2025-02-01 14:57:12 +0800

    Test: Config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    localPatterns: [
-      {
-        pathname: '/assets/gallery/**',
-        search: '',
-      },
-      {
-        pathname: '/assets/projects/**',
-        search: '',
-      },
-      {
-        pathname: '/assets/resume/**',
-        search: '',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
